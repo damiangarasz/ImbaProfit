0c58f9adb1d9a6bb7cf14566e788702a3d8a3604
damiangarasz <damian.garasz@gmail.com>
2024-02-17 15:57:15 +0100

    commit commit lol

--- Blog/poker-migration-series-thailand/podmianka.js
+++ Blog/poker-migration-series-thailand/podmianka.js
@@ -18,9 +18,11 @@ console.log(szerokosc);
 function doStuff() {
   if (szerokosc <= 550) {
     lol.setAttribute("src", "mobile-1.jpg");
-  } else if (szerokosc <= 900 && szerokosc > 550) {
+  } else if (szerokosc <= 1000 && szerokosc > 551) {
     lol.setAttribute("src", "tablet.jpg");
   } else {
     lol.setAttribute("src", "BG1.jpg");
   }
 }
+
+doStuff();
