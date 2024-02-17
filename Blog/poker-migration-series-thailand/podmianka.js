2d119346113f981a11d22b08eac7557476951be5
damiangarasz <damian.garasz@gmail.com>
2024-02-17 15:13:31 +0100

    dodany JS

---
+++ Blog/poker-migration-series-thailand/podmianka.js
@@ -0,0 +1,26 @@
+const lol = document.querySelector(".img-mobile");
+let szerokosc =
+  window.innerWidth ||
+  document.documentElement.clientWidth ||
+  document.body.clientWidth;
+
+window.addEventListener("resize", (event) => {
+  szerokosc =
+    window.innerWidth ||
+    document.documentElement.clientWidth ||
+    document.body.clientWidth;
+
+  doStuff();
+});
+
+console.log(szerokosc);
+
+function doStuff() {
+  if (szerokosc <= 550) {
+    lol.setAttribute("src", "mobile-1.jpg");
+  } else if (szerokosc <= 900 && szerokosc > 550) {
+    lol.setAttribute("src", "tablet.jpg");
+  } else {
+    lol.setAttribute("src", "BG1.jpg");
+  }
+}
