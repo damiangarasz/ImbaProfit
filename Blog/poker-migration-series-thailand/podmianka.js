397d03678e3c0ab487937c26f1e562a56d08524f
damiangarasz <damian.garasz@gmail.com>
2024-02-22 15:44:18 +0100

    comit woow

--- Blog/poker-migration-series-thailand/podmianka.js
+++ Blog/poker-migration-series-thailand/podmianka.js
@@ -1,4 +1,7 @@
 const lol = document.querySelector(".img-mobile");
+const lol2 = document.querySelector(".mobile-2");
+const lol3 = document.querySelector(".mobile-3");
+
 let szerokosc =
   window.innerWidth ||
   document.documentElement.clientWidth ||
@@ -18,10 +21,16 @@ console.log(szerokosc);
 function doStuff() {
   if (szerokosc <= 550) {
     lol.setAttribute("src", "mobile-1.jpg");
+    lol2.setAttribute("src", "mobile-22.webp");
+    lol3.setAttribute("src", "");
   } else if (szerokosc <= 1000 && szerokosc > 551) {
     lol.setAttribute("src", "tablet.jpg");
+    lol2.setAttribute("src", "");
+    lol3.setAttribute("src", "tablet-desktop.jpg");
   } else {
     lol.setAttribute("src", "BG1.jpg");
+    lol2.setAttribute("src", "");
+    lol3.setAttribute("src", "tablet-desktop.jpg");
   }
 }
 
