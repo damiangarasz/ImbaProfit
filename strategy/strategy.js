0697ef2e7f95a9d5d0b9bce69b593d3e16a0951c
damiangarasz <damian.garasz@gmail.com>
2024-03-04 19:32:05 +0100

    lol wow

--- strategy/strategy.js
+++ strategy/strategy.js
@@ -1,13 +1,30 @@
 class obj {
-    constructor(img, url, title) {
-        this.img = img;
-        this.url = url;
-        this.title = title;
-    }
+  constructor(img, url, title) {
+    this.img = img;
+    this.url = url;
+    this.title = title;
+    this.pusz();
+  }
+
+  pusz() {
+    arrMiniaturek.push(this);
+  }
 }
+
+const arrMiniaturek = [];
 
 const basicPokerMath = new obj(
   "./min/basicpokermath.webp",
   "https://youtu.be/aVh7rUAkSlM",
   "Basic Poker Math"
 );
+
+document.addEventListener("DOMContentLoaded", () => {
+  const chwytak = document.querySelector(".propozycje");
+  for (let n = 0; n < arrMiniaturek.length; n++) {
+    const miniturka = document.createElement("img");
+    miniturka.setAttribute("src", arrMiniaturek[n].img);
+    console.log(miniturka);
+    chwytak.appendChild(miniturka);
+  }
+});
