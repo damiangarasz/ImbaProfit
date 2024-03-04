fd91713d7cdc70b592d195594a5ecc7da7029adf
damiangarasz <damian.garasz@gmail.com>
2024-03-04 17:23:14 +0100

    do jutra

---
+++ strategy/strategy.js
@@ -0,0 +1,13 @@
+class obj {
+    constructor(img, url, title) {
+        this.img = img;
+        this.url = url;
+        this.title = title;
+    }
+}
+
+const basicPokerMath = new obj(
+  "./min/basicpokermath.webp",
+  "https://youtu.be/aVh7rUAkSlM",
+  "Basic Poker Math"
+);
