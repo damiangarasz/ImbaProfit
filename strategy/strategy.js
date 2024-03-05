1aebe6db6aaaab47b9ca6c5bfcb27fb2087b18af
damiangarasz <damian.garasz@gmail.com>
2024-03-05 18:00:43 +0100

    video działa

--- strategy/strategy.js
+++ strategy/strategy.js
@@ -62,22 +62,100 @@ const lp2 = new obj(
 );
 
 document.addEventListener("DOMContentLoaded", () => {
-
   const chwytak = document.querySelector(".propozycje");
 
   for (let n = 0; n < arrMiniaturek.length; n++) {
-
     const miniturka = document.createElement("img");
     miniturka.setAttribute("src", arrMiniaturek[n].img);
     miniturka.classList.add("miniaturka");
-    console.log(miniturka);
+    miniturka.setAttribute("data-value", arrMiniaturek[n].title);
     chwytak.appendChild(miniturka);
-
   }
-  //TODO dodać event listener do buttonów i zrobić scrolla na miniaturkach
-  const lol = document.querySelectorAll(".button-scroll")
-  document.addEventListener()
 
+  const chwytakMiniaturki = document.querySelectorAll(".miniaturka");
+
+  for (let n of chwytakMiniaturki) {
+    n.addEventListener("click", () => {
+      const lapacz = n.getAttribute("data-value");
+      const inframe = document.querySelector(".inframe");
+
+      switch (lapacz) {
+        case "Basic Poker Math":
+          inframe.setAttribute(
+            "src",
+            "https://www.youtube.com/embed/aVh7rUAkSlM?si=oLUQo6bj5dpSfCMK"
+          );
+          break;
+        case "Fishmaster BTN":
+          inframe.setAttribute(
+            "src",
+            "https://www.youtube.com/embed/fpuZyS3Dk9k?si=UENflYbGS8Eo3WTy"
+          );
+          break;
+        case "Fishmaster HUBB":
+          inframe.setAttribute(
+            "src",
+            "https://www.youtube.com/embed/4BSgk9zXvYc?si=a-j1flCStcSWZUo7"
+          );
+          break;
+        case "Fishmaster HUSB":
+          inframe.setAttribute(
+            "src",
+            "https://www.youtube.com/embed/OS4Jjs2j1Mk?si=mKsfVE7M5Mvhdy7J"
+          );
+          break;
+        case "Fishmaster SBvBB":
+          inframe.setAttribute(
+            "src",
+            "https://www.youtube.com/embed/cC-Ss0DagAQ?si=bI-yOWb_mkfAtZWm"
+          );
+          break;
+        case "GTO Preflop Adjust":
+          inframe.setAttribute(
+            "src",
+            "https://www.youtube.com/embed/pNXZi_0MGJY?si=sHmnvnWtlEyGzn2r"
+          );
+          break;
+        case "Liveplay 1":
+          inframe.setAttribute(
+            "src",
+            "https://www.youtube.com/embed/sohv1Oba_Xo?si=SxI_SLMBu2f02ift"
+          );
+          break;
+        case "Liveplay 2":
+          inframe.setAttribute(
+            "src",
+            "https://www.youtube.com/embed/SUMuV9wTIRo?si=TmMS__BSXyFX30Uo"
+          );
+          break;
+      }
+    });
+  }
+
+  const lol = document.querySelectorAll(".button-scroll");
+  let pozycja = 0;
+
+  for (let n of lol) {
+    n.addEventListener("click", (event) => {
+      const lol = event.target.value;
+      console.log(lol);
+      const element = document.querySelector(".propozycje");
+
+      if (lol == "right") {
+        if (pozycja >= arrMiniaturek.length * 150) {
+          pozycja = arrMiniaturek.length * 150;
+        } else {
+          pozycja += 300;
+        }
+      } else if (lol == "left") {
+        if (pozycja == 0) {
+          pozycja = 0;
+        } else {
+          pozycja -= 300;
+        }
+      }
+
+      element.scrollTo({ left: pozycja, behavior: "smooth" });
+    });
+  }
 });
-
-
